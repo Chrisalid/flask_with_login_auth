884ea91d449b46ad2f0e0ad8779709320d930869
1820600 <1820600@ffb.local>
2021-10-20 21:36:47 -0300

    add template and static files

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('home_page.html')
 
 
 if __name__ == '__main__':
