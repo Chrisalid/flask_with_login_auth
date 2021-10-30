0efcb45fb9ff969f46e6f8943644491b1684f121
Chrisopher <christopherfeitosa17@hotmail.com>
2021-10-30 00:09:18 -0300

    add new routes to make login and registration in website

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
+users = dict()
 
 
 @app.route('/', methods=['GET'])
 def home():
     return render_template('home_page.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        return 'Pagina Sendo Criada'
+    return render_template('register.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        request.jsonify()
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
